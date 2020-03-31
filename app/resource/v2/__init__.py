2365126596369cde553c985c225774a2897194e3
Guo Chuanxiang <guochuanxiang@datagrand.com>
2020-03-31 10:58:40 +0800

    resolve conflicts

--- app/resource/v2/__init__.py
+++ app/resource/v2/__init__.py
@@ -2,6 +2,5 @@ from flask import Blueprint
 
 blueprint = Blueprint('v2', __name__, url_prefix='/v2')
 
-from app.entity import *
-from . import dashboard, other
+from . import dashboard, other, model
 
