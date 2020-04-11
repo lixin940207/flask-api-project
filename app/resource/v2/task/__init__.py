3cd09fb0d40cf7f30755c09cb0cf492e665ffa9c
Guo Chuanxiang <guochuanxiang@datagrand.com>
2020-04-11 18:04:12 +0800

    /reject_manual_task/<id>

--- app/resource/v2/task/__init__.py
+++ app/resource/v2/task/__init__.py
@@ -2,4 +2,4 @@ from flask_restful import Api
 from app.resource.v2 import blueprint
 api = Api(blueprint, prefix='/task', decorators=[])
 
-from app.resource.v2.task import manual
+from app.resource.v2.task import manual, machine
