c729543c0cf00815c708364ebc4e5dc85f1e35cf
Li Xin <lixin@datagrand.com>
2020-04-14 17:58:21 +0800

    fix /async

--- app/model/mark_job_model.py
+++ app/model/mark_job_model.py
@@ -32,10 +32,10 @@ class MarkJobModel(BaseModel, ABC):
             if key in accept_keys:
                 q = q.filter(getattr(MarkJob, key) == val)
         # Order by key
-        q = q.order_by(order_by)
-        # Descending order
         if order_by_desc:
-            q = q.desc()
+            q = q.order_by(getattr(MarkJob, order_by).desc())
+        else:
+            q = q.order_by(getattr(MarkJob, order_by))
         q = q.offset(offset).limit(limit)
         return q.all()
 
