3b12cf8efc455a15c345d92d5008318880d1bb03
Li Xin <lixin@datagrand.com>
2020-04-15 20:49:02 +0800

    fix

--- app/service/model_service.py
+++ app/service/model_service.py
@@ -157,7 +157,8 @@ class ModelService:
         train_job = TrainJobModel().get_by_id(_id)
         _, train_job.train_list = TrainTaskModel().get_by_filter(limit=99999, train_job_id=train_job.train_job_id)
         _, model_evaluate_list = EvaluateTaskModel().get_by_train_job_id(train_job_id=train_job.train_job_id, evaluate_task_status=int(StatusEnum.success))
-        train_job.model_evaluate = model_evaluate_list[0]
+        if model_evaluate_list:
+            train_job.model_evaluate = model_evaluate_list[0]
         return train_job
 
     @staticmethod
