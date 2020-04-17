0be54f22bd0520d16a425d83145dba1c79089717
Li Xin <lixin@datagrand.com>
2020-04-17 14:45:28 +0800

    set default value to doc_status

--- app/entity/doc.py
+++ app/entity/doc.py
@@ -1,3 +1,4 @@
+from app.common.common import StatusEnum
 from app.common.extension import db
 from app.entity.base import BaseEntity
 
@@ -6,4 +7,4 @@ class Doc(BaseEntity):
     doc_id = db.Column(db.Integer(), primary_key=True)
     doc_unique_name = db.Column(db.String(64), nullable=False)
     doc_raw_name = db.Column(db.String(255), nullable=False)
-    doc_status = db.Column(db.Integer(), db.ForeignKey("status.status_id"), nullable=False)
+    doc_status = db.Column(db.Integer(), db.ForeignKey("status.status_id"), default=StatusEnum.processing.value, nullable=False)
